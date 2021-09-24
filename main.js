11cd139b2df57849bb028229c70a552c7952c105
Sounthida Kong <sounthikg@gmail.com>
2021-09-24 16:11:29 -0400

    menu_hamburger_javascript

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 import './styles.scss'
+
+var hamburgerBtn = document.querySelector('.navicon');
+var hamburgerMenu = document.querySelector('.menu__hamburger');
+
+
+hamburgerBtn.addEventListener('click', function() {
+
+    var hamDisplay = hamburgerMenu.style.display;
+
+    if (hamDisplay == 'none') {
+
+        hamburgerMenu.style.display = 'flex';
+        hamburgerMenu.style.height = '100vh';
+    }
+
+    else {
+        hamburgerMenu.style.display = 'none';
+        hamburgerMenu.style.height = '0vh';
+    }
+    
+})
