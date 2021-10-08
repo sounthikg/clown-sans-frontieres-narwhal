6133a8ab82fc50f72fe4fea9d062a12d2a449524
Sounthida Kong <sounthikg@gmail.com>
2021-10-08 11:40:38 -0400

    footer-md

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 import './styles.scss'
+import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+
+Swiper.use([Navigation, Pagination, Autoplay]);
+
+// init Swiper:
+//const swiper = new Swiper(...);
+
 
 var hamburgerBtn = document.querySelector('.navicon');
 var hamburgerMenu = document.querySelector('.menu__hamburger');
@@ -21,3 +32,31 @@ hamburgerBtn.addEventListener('click', function() {
     }
     
 })
+
+const swiper = new Swiper('.pied-page__swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  speed: 600,
+
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
