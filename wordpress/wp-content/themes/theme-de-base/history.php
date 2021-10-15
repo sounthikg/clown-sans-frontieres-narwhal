9dc2db103f5d740163b7387b14b6be5d10d94b32
Sounthida Kong <sounthikg@gmail.com>
2021-10-15 15:52:18 -0400

    yes

--- wordpress/wp-content/themes/theme-de-base/history.php
+++ wordpress/wp-content/themes/theme-de-base/history.php
@@ -1,6 +1,6 @@
 <?php 
 /**
- * 	Template Name: À propos
+ * 	Template Name: History
  * 	Identique à page, mais avec une barre latérale
  */
 
--- wordpress/wp-content/themes/theme-de-base/home.php
+++ wordpress/wp-content/themes/theme-de-base/home.php
@@ -1,6 +1,6 @@
 <?php 
 /**
- * 	Template Name: À propos
+ * 	Template Name: Home
  * 	Identique à page, mais avec une barre latérale
  */
 
