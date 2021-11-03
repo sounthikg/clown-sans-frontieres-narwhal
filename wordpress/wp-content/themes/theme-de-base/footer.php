c7b5d5d2ae6d7808879a6222939fc36f623b898e
Sounthi <sounthikg@gmail.com>
2021-11-02 23:33:33 -0400

    Simon is OP and fixed everything

--- wordpress/wp-content/themes/theme-de-base/footer.php
+++ wordpress/wp-content/themes/theme-de-base/footer.php
@@ -91,6 +91,9 @@
 	
 </footer>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
 <?php wp_footer(); 
 /* Espace où WordPress peut insérer des fichiers .js et autres. Par exemple pour des extensions (plugins). 
 	 Si vous enlevez cette fonction, vous désactiverez du même coup toutes vos extensions (plugins) 🤷. 
