31e611b17eea54b8ef03f7a091c768810eb32f3d
Sounthida Kong <70409867+Sounthi@users.noreply.github.com>
2021-11-26 15:49:19 -0500

    push

--- wordpress/wp-content/themes/theme-de-base/team.php
+++ wordpress/wp-content/themes/theme-de-base/team.php
@@ -212,7 +212,7 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
 	
 
     <script>
-      fetch("<?php echo get_home_path()?>/wp-json/wp/v2/membres?_embed&orderby=date&order=asc&per_page=13")
+      fetch("<?php echo get_site_url(); ?>/wp-json/wp/v2/membres?_embed&orderby=date&order=asc&per_page=13")
 .then(response => response.json())
 .then(data => { 
 	console.log(data);
